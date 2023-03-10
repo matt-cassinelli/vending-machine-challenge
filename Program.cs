a38f6a7194bde141a72dc554e10721dfcc6f809c
matt-cassinelli <67283034+matt-cassinelli@users.noreply.github.com>
2023-03-10 14:05:40 +0000

    Accept and store coin

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿var vendingMachine = new VendingMachine();
+
+while (true)
+{
+    Console.WriteLine("PLEASE ENTER 1p, 2p, 5p, 10p, 20p, 50p, £1 OR £2");
+    string potentialCoin = Console.ReadLine();
+    vendingMachine.InsertCoin(potentialCoin);
+}
+
+public class VendingMachine
+{
+    public List<string> CoinsInCredit = new List<string>();
+
+    public void InsertCoin(string name)
+    {
+        CoinsInCredit.Add(name);
+    }
+}
