93cbbf9fd9698c421a40c9d2779f2ba92292494d
matt-cassinelli <67283034+matt-cassinelli@users.noreply.github.com>
2023-03-10 14:30:30 +0000

    Improve UI, add main loop

--- Program.cs
+++ Program.cs
@@ -1,13 +1,36 @@
-﻿using VendingMachineApp;
+﻿// UI Layer
+
+using VendingMachineApp;
 
 var vendingMachine = new VendingMachine();
 
 while (true)
 {
-    Console.WriteLine("PLEASE ENTER 1p, 2p, 5p, 10p, 20p, 50p, £1 OR £2");
+    Console.Clear();
+    Console.WriteLine("1) INSERT COIN");
+    Console.WriteLine("2) SELECT PRODUCT");
+    Console.WriteLine("3) RETURN MY COINS");
+    Console.WriteLine();
     if (vendingMachine.ValueInCredit > 0)
         Console.WriteLine($"CREDIT: {vendingMachine.ValueInCredit.ToString("C")}");
+    else
+        Console.WriteLine("INSERT COIN");
 
-    string potentialCoin = Console.ReadLine();
-    vendingMachine.InsertCoin(potentialCoin);
+    string input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            Console.Clear();
+            Console.WriteLine("PLEASE ENTER 1p, 2p, 5p, 10p, 20p, 50p, £1 OR £2");
+            string potentialCoin = Console.ReadLine();
+            vendingMachine.InsertCoin(potentialCoin);
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+        default:
+            break;
+    }
 }
